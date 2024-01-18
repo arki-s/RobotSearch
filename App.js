6ddd50d40c60a10608355bb4e1d91a8cd3d083c0
exec zsh <wind10cloud@gmail.com>
2024-01-18 17:57:01 +0900

    added login page style

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './App/Screens/LoginScreen/Login';
+import { useFonts } from 'expo-font';
+// import * as SplashScreen from 'expo-splash-screen';
+// import { useCallback } from 'react';
+
+// SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'kaisei':require("./assets/fonts/KaiseiOpti-Regular.ttf"),
+    'kaisei-bold':require("./assets/fonts/KaiseiOpti-Bold.ttf"),
+  })
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
+
+  // if (!fontsLoaded) {
+  //   return null;
+  // }
+
   return (
     <View style={styles.container}>
      <Login />
--- App/Screens/LoginScreen/Login.jsx
+++ App/Screens/LoginScreen/Login.jsx
@@ -1,15 +1,75 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
 import React from "react";
+import Colors from "../../Utils/Colors";
 
 export default function Login() {
   return (
-    <View>
-      <Text>ロボットを身近に</Text>
-      <Text>
-        ロボットの活用が進んできていますが、個人単位での活用はまだまだ発展途上。これはロボットをより身近に感じ、新たな社会への一歩を踏み出すためのサービスです。
-      </Text>
-      <Text>登録する</Text>
-      <Text>ログイン</Text>
+    <View style={{ alignItems: "center" }}>
+      {/* <View style={styles.mainContainer}> */}
+      <Text style={styles.concept}>ロボットを身近に</Text>
+      <Image
+        source={require("../../../assets/images/connection.jpg")}
+        style={styles.image}
+      />
+      <View style={styles.container}>
+        <Text style={styles.message}>
+          ロボットの活用が進んできていますが、個人単位での活用はまだまだ発展途上。これはロボットをより身近に感じ、新たな社会への一歩を踏み出すためのサービスです。
+        </Text>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={styles.btn}>登録する</Text>
+          <Text style={styles.btn}>ログイン</Text>
+        </View>
+      </View>
     </View>
+    // </View>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    marginTop: -100,
+    height: 410,
+    width: 410,
+    objectFit: "contain",
+  },
+  concept: {
+    marginTop: 100,
+    textAlign: "center",
+    fontFamily: "kaisei",
+    fontSize: 36,
+    height: "20%",
+    flex: 1,
+  },
+  container: {
+    backgroundColor: Colors.PRIMARY,
+    width: "102%",
+    height: "70%",
+    padding: 20,
+    marginTop: -20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    flex: 1,
+  },
+  message: {
+    fontFamily: "kaisei",
+    color: Colors.WHITE,
+    fontSize: 18,
+    marginBottom: 25,
+  },
+  btn: {
+    fontFamily: "kaisei",
+    fontSize: 22,
+    padding: 10,
+    paddingBottom: 15,
+    marginHorizontal: 5,
+    color: Colors.BLACK,
+    borderRadius: 99,
+    backgroundColor: Colors.SECONDARY_LIGHT,
+  },
+});
