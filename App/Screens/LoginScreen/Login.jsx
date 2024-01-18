5584ea2a192c61ae1ac9a78df47ebb5160bdce40
exec zsh <wind10cloud@gmail.com>
2024-01-18 12:00:33 +0900

    added login

---
+++ App/Screens/LoginScreen/Login.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Login() {
+  return (
+    <View>
+      <Text>ロボットを身近に</Text>
+      <Text>
+        ロボットの活用が進んできていますが、個人単位での活用はまだまだ発展途上。これはロボットをより身近に感じ、新たな社会への一歩を踏み出すためのサービスです。
+      </Text>
+      <Text>登録する</Text>
+      <Text>ログイン</Text>
+    </View>
+  );
+}
