2e93a111f830ab94c45c9d19ac841bd192e027b6
exec zsh <wind10cloud@gmail.com>
2024-01-23 17:14:29 +0900

    added tab nav

---
+++ App/Navigations/TabNavigation.jsx
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../Screens/HomeScreen/HomeScreen";
+import BookingScreen from "../Screens/BookingScreen/BookingScreen";
+import ProfileScreen from "../Screens/ProfileScreen/ProfileScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="home" component={HomeScreen}></Tab.Screen>
+      <Tab.Screen name="booking" component={BookingScreen}></Tab.Screen>
+      <Tab.Screen name="profile" component={ProfileScreen}></Tab.Screen>
+    </Tab.Navigator>
+  );
+}
