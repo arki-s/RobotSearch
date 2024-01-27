906f281ac946dcc62547319dfb09209f888e82d2
exec zsh <wind10cloud@gmail.com>
2024-01-27 13:47:34 +0900

    created again

--- App.js
+++ App.js
@@ -1,39 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './App/Screens/LoginScreen/Login';
-import { useFonts } from 'expo-font';
-import { ClerkProvider } from "@clerk/clerk-expo";
-import { CLERK_PUBLISHABLE_KEY } from '@env';
-// import * as SplashScreen from 'expo-splash-screen';
-// import { useCallback } from 'react';
-
-// SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'kaisei':require("./assets/fonts/KaiseiOpti-Regular.ttf"),
-    'kaisei-bold':require("./assets/fonts/KaiseiOpti-Bold.ttf"),
-  })
-
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (fontsLoaded) {
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [fontsLoaded]);
-
-  // if (!fontsLoaded) {
-  //   return null;
-  // }
-
-
-
   return (
-    // <ClerkProvider>
     <View style={styles.container}>
-     <Login />
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-    // </ClerkProvider>
   );
 }
 
