a1eba912bf83b9927bd1c471198573b670ca6663
exec zsh <wind10cloud@gmail.com>
2024-01-30 16:23:53 +0900

    Added files

---
+++ App/Screens/RobotDetails.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function RobotDetails() {
+  return (
+    <View>
+      <Text>RobotDetails</Text>
+    </View>
+  )
+}
---
+++ App/Screens/Robots.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Robots() {
+  return (
+    <View>
+      <Text>Robots</Text>
+    </View>
+  )
+}
