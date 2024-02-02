ee3e86adfcac18b8457f04286bb59ec175a27f34
exec zsh <wind10cloud@gmail.com>
2024-02-02 15:00:35 +0900

    added clerk and dotenv

--- App.tsx
+++ App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import { ClerkProvider, useSignUp } from "@clerk/clerk-expo";
+import { CLERK_PUBLISHABLE_KEY } from '@env';
 
 
 export default function App() {
   return (
-    <GestureHandlerRootView>
-      <NavigationContainer>
-        <Text>test</Text>
-      </NavigationContainer>
-    </GestureHandlerRootView>
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+      <GestureHandlerRootView>
+        <NavigationContainer>
+          <Text>test</Text>
+        </NavigationContainer>
+      </GestureHandlerRootView>
+    </ClerkProvider>
   );
 }
