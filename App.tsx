722c7b7fab365505d361cb35b242071c463a3bda
exec zsh <wind10cloud@gmail.com>
2024-02-04 10:32:57 +0900

    added signin and logout

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { ClerkProvider, useSignUp } from "@clerk/clerk-expo";
+import { ClerkProvider, SignedIn, SignedOut, useSignUp } from "@clerk/clerk-expo";
 import { CLERK_PUBLISHABLE_KEY } from '@env';
 
 
 export default function App() {
   return (
-    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
-      <GestureHandlerRootView>
-        <NavigationContainer>
-          <Text>test</Text>
-        </NavigationContainer>
-      </GestureHandlerRootView>
+    <ClerkProvider publishableKey={(CLERK_PUBLISHABLE_KEY).trim()}>
+      <SafeAreaView>
+        <GestureHandlerRootView>
+          <NavigationContainer>
+            <SignedIn>
+              <Text>You are Signed in</Text>
+            </SignedIn>
+            <SignedOut>
+              <Text>You are Signed out</Text>
+            </SignedOut>
+          </NavigationContainer>
+        </GestureHandlerRootView>
+      </SafeAreaView>
     </ClerkProvider>
   );
 }
